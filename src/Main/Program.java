fa5b15f965cf5a677c342d63b62b22a94f63d3f4
nina_smirnova <ilovescience@bk.ru>
2019-12-20 14:31:57 +0300

    added iconimage of application

--- src/Main/Program.java
+++ src/Main/Program.java
@@ -1,13 +1,9 @@
 package Main;
 
 import javax.swing.*;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 
 public class Program {
 
@@ -29,6 +25,9 @@ public class Program {
         frame.setVisible(true);
 
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        ImageIcon icon = new ImageIcon("paintbrush.png");
+        frame.setIconImage(icon.getImage());
 
         GridBagLayout layout = new GridBagLayout();
         frame.setLayout(layout);
